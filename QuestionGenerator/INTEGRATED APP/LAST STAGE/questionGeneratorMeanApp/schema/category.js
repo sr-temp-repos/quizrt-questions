a3ec2f47924dc94855c17c1e117c3020ca069653
ngautam0 <ngautam0@live.com>
2016-02-26 17:32:01 +0530

    Final Push For Amazon Deployment

--- QuestionGenerator/INTEGRATED APP/LAST STAGE/questionGeneratorMeanApp/schema/category.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var categorySchema = {
-  _id: { type: String, required: true},
-  name: { type: String, required: true},
-  imageUrl: { type: String },
-};
-
-module.exports = new mongoose.Schema(categorySchema);
